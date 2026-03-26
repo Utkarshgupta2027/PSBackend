bcbdc3ab0d561bf6f3eb9566aa9bf18414fe5c48
Utkarsh Gupta <gutkarsh702@gmail.com>
2026-03-26 16:17:23 +0530

    changed base url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'payment-system-production-4332.up.railway.app';
 
 const api = axios.create({
   baseURL: BASE_URL,
