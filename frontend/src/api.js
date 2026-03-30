2ae027006515286d72ac4bec0503b34ff6cb662d
Utkarsh Gupta <gutkarsh702@gmail.com>
2026-03-30 22:43:19 +0530

    base api changed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,17 +1,9 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
 
+console.log("BASE_URL:", BASE_URL); // 👈 add this
+
 export const registerUser = (data) => {
   return fetch(`${BASE_URL}/user/register`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-};
-
-export const loginUser = (data) => {
-  return fetch(`${BASE_URL}/user/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
